3bdab71de2c44dc3b5974fef6cb8b96e11848fa9
pinkyMan79 <48048748+pinkyMan79@users.noreply.github.com>
2023-06-16 14:00:00 +0300

    MAIN: Initial commit

---
+++ src/main/java/one/terenin/MusAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package one.terenin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusAuthenticationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusAuthenticationServiceApplication.class, args);
+    }
+
+}
